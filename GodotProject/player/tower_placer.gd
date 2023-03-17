d2ef471933c4c84818873adf76d9dbd96413ef52
Ikuti <m.j.olkiewicz@gmail.com>
2023-03-17 02:27:51 +0100

    Tower placer added

--- GodotProject/player/tower_placer.gd
+++ GodotProject/player/tower_placer.gd
@@ -1,5 +1,7 @@
 extends Node
 
+signal tower_placed(tower)
+
 @export var floor_tile_map : TileMap
 @export var selection_visual : Sprite2D
 @export var offset : Vector2 = Vector2.ONE
@@ -7,13 +9,20 @@ extends Node
 var can_place_towers := false
 
 var _current_tower : Tower
+var _current_cell := Vector2i.ZERO
 
 const AVAILABLE_CELL_ID := 0
 const INVAILD_CELL_ID := 1
 
 func _ready():
-	selection_visual.hide()
-
+	set_process_unhandled_input(false)
+	
+	setup_available_cells([Vector2(12, 7)])
+	
+	var tower = load("res://tower_weapon/tower.tscn").instantiate()
+	
+	add_new_tower(tower)
+	
 func _input(event):
 	pass
 	
@@ -40,6 +49,44 @@ func add_new_tower(tower: Tower) -> void:
 	
 	set_process_unhandled_input(true)
 	
+	_snap_tower_to_grid()
+	
+func _unhandled_input(event):
+	if event is InputEventMouseMotion:
+		_snap_tower_to_grid()
+		
+	if event.is_action_pressed("tower_placement"):
+		_place_tower()
+	
+func _place_tower():
+	set_process_unhandled_input(false)
+	
+	if not is_cell_placeable(_current_cell):
+		_current_tower.queue_free()
+		_current_tower = null
+		return
+		
+	set_cell_unplaceable(_current_cell)
+	
+	emit_signal("tower_placed", _current_tower)
+	_current_tower.connect("sold", _on_Tower_sold)
+	_current_tower = null
+	
+	
+func _on_Tower_sold(_price: int, place: Vector2):
+	set_cell_placeable(floor_tile_map.local_to_map(place))
+
+func _snap_tower_to_grid():
+	_current_cell = floor_tile_map.local_to_map(floor_tile_map.get_local_mouse_position() + offset)
+	_current_tower.global_position = floor_tile_map.map_to_local(_current_cell)
+	
+	if not is_cell_placeable(_current_cell):
+		_current_tower.modulate = Color.DARK_RED
+	else:
+		_current_tower.modulate = Color.WHITE
+		
+
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	var hovered_cell = floor_tile_map.local_to_map(floor_tile_map.get_local_mouse_position() + offset)
